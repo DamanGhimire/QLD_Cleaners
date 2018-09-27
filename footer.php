e8ce3dd53e7b219f93ee886b7d742622c914cc72
BiswasGurung <43643870+BiswasGurung@users.noreply.github.com>
2018-09-28 01:15:30 +1000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,57 @@
+<div class="footer">
+    <div class="container-fluid padding">
+        <div class="row text-center" >
+            <div class="col-md-4">
+                <hr class="light">
+                <h4>Contact us</h4>
+                <hr class="light">
+                <p>0430 056 485</p>
+                <p>info@qldcleaners.com.au</p>
+                <p>Queensland Cleaners</p>
+                <p>Brisbane, Gold Coast</p>
+
+            </div>
+            <div class="col-md-4";>
+                <hr class="light">
+                <h4>Our Hours</h4>
+                <hr class="light">
+                <p>Monday to Friday: 7 a.m - 7 p.m</p>
+                <p>Saturday: 8 a.m - 6 p.m </p>
+                <p>Sunday: 9 a.m - 5 p.m</p>
+            </div>
+            <div class="col-md-4">
+                <hr class="light">
+                <h4>Service Area</h4>
+                <hr class="light">
+                <p>Inner Brisbane</p>
+                <p>Northern Suburbs</p>
+                <p>Sothern Suburbs</p>
+                <p>Gold Cost</p>
+            </div>
+            
+                <div class="col-12">
+                    <div class="social padding">
+                        <a href="https://www.facebook.com/shiny.cleaners.1?fref=search/"><i class="fab fa-facebook"></i></a>
+                        <a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.gmail.com"><i class="fab fa-google-plus-g"></i></a>
+                        <a href="https://www.instagram.com/bissosh_gurung/"><i class="fab fa-instagram"></i></a>
+                        <a href="https://www.youtube.com/watch?v=1gHwMppb1ds"><i class="fab fa-youtube"></i></a>
+                    </div>
+                </div>
+            <div class="col-12">
+                <hr class="light">
+                <h5>&copy;QLD Cleaners, 2018</h5>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+</body>
+</html>
