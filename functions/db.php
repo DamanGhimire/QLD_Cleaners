dfa801276d9141f8425c5aa34634828230af5ce5
DamanGhimire <31787746+DamanGhimire@users.noreply.github.com>
2018-09-30 14:50:24 +1000

    Add files via upload

---
+++ functions/db.php
@@ -0,0 +1,56 @@
+<?php
+//$db_name='qld_cleaners';
+//$host='localhost';
+//$user='root';
+//$password='';
+//
+//	$connection = @mysql_connect($host, $user,$password);
+//	if(!$connection)
+//	{
+//		die("Connection not successful".mysql_error());
+//	}
+//
+//	$db_select = mysql_select_db($db_name, $connection);
+//	if(!$db_select)
+//	{
+//		die("Database selection not successful".mysql_error());
+//	}
+//
+//?>
+
+<?php
+//
+//// Create connection
+//
+//function db_connect()
+//{$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$db="qld_cleaners";
+//	$conn = new mysqli($servername, $username, $password, $db);
+//	if($conn -> connect_error) {
+//		die('Connect Error: ' . $conn -> connect_error);
+//	}
+//	return $conn;
+//}
+//// Check connection
+//
+//// echo "Connected successfully";
+//?>
+
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'ictatjcu_clean');
+define('DB_PASSWORD', '123zxc');
+define('DB_NAME', 'ictatjcu_clean');
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
