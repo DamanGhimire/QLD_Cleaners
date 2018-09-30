81da6c9f2747f5b3d9fe97cacab0ff5cd5946148
BiswasGurung <43643870+BiswasGurung@users.noreply.github.com>
2018-09-30 14:47:40 +1000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,54 @@
+<div class="footer">
+    <div class="container-fluid padding">
+        <div class="row text-center" >
+            <div class="col-md-4">
+                <hr class="light">
+                <h4>Contact us</h4>
+                <hr class="light">
+                    0430 056 485<br>
+                    info@qldcleaners.com.au<br>
+                    Queensland Cleaners<br>
+                    Brisbane, Gold Coast<br>
+            </div>
+            <div class="col-md-4";>
+                <hr class="light">
+                <h4>Our Hours</h4>
+                <hr class="light">
+                    Monday to Friday: 7 a.m - 7 p.m<br>
+                    Saturday: 8 a.m - 6 p.m<br>
+                    Sunday: 9 a.m - 5 p.m<br>
+            </div>
+            <div class="col-md-4">
+                <hr class="light">
+                <h4>Service Area</h4>
+                <hr class="light">
+                    Inner Brisbane<br>
+                    Northern Suburbs<br>
+                    Sothern Suburbs<br>
+                    Gold Cost<br>
+            </div>
+
+                <div class="col-12">
+                    <div class="social padding">
+                        <a href="https://www.facebook.com/shiny.cleaners.1?fref=search/"><i class="fab fa-facebook"></i></a>
+                        <a href="https://www.gmail.com"><i class="fab fa-google-plus-g"></i></a>
+                        <a href="https://www.youtube.com/watch?v=1gHwMppb1ds"><i class="fab fa-youtube"></i></a>
+                    </div>
+                </div>
+            <div class="col-12">
+                <hr class="light">
+                <h5>&copy;QLD Cleaners, 2018</h5>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+</body>
+</html>
